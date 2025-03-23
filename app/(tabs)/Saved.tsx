471617b38eeb3dccc9ee26e8ace408fdd2f9ab72
Md. Abdullah Al Hasan <hasan.cse.iub@gmail.com>
2025-03-24 02:59:49 +0600

    fffff

---
+++ app/(tabs)/Saved.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Saved = () => {
+  return (
+    <View>
+      <Text>S</Text>
+    </View>
+  );
+};
+
+export default Saved;
+
+const styles = StyleSheet.create({});
